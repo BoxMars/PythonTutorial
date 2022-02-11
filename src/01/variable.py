ddba74844fd48745d05c5a34abdd236e75a11ccb
boxmars <2803726925@qq.com>
2022-02-11 21:09:38 +0800

    update

---
+++ src/01/variable.py
@@ -0,0 +1,16 @@
+a = 10.0 #浮点数
+print ("type  of a is {}".format(type(a)))
+print ("value of a is {}\n".format(a))
+
+
+b = int(a) #将 a转换成整数形式 int：integer
+print ("type  of b is {}".format(type(b)))
+print ("value of b is {}\n".format(b))
+
+c = 1000000*100000 # 长整型
+print ("type  of c is {}".format(type(c)))
+print ("value of c is {}\n".format(c))
+
+d = float(c) # 将 c转换成浮点数
+print ("type  of d is {}".format(type(d)))
+print ("value of d is {}\n".format(d))
