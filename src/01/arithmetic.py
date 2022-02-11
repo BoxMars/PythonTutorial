d8232179bd47337acba710cb59dd40e7832fd625
boxmars <2803726925@qq.com>
2022-02-11 21:50:47 +0800

    update

---
+++ src/01/arithmetic.py
@@ -0,0 +1,34 @@
+a = 10
+b = 3
+
+print("value of a is {}".format(a))
+print("value of b is {}\n".format(b))
+
+c = a + b
+print("c = a + b")
+print("value of c is {}\n".format(c))
+
+d = a - b
+print("d = a - b")
+print("value of d is {}\n".format(d))
+
+e = a / b
+print("e = a / b")
+print("value of e is {}\n".format(e))
+
+f = b / a
+print("f = b / a")
+print("value of f is {}\n".format(f))
+
+g = a ** b
+print("g = a ** b")
+print("value of g is {}\n".format(g))
+
+h = a % b
+print("h = a % b")
+print("value of h is {}\n".format(h))
+
+i = a // b
+print("i = a // b")
+print("value of i is {}".format(i))
+
